b05797352e2674b593d1ebe6aa815c414255688c
wahyuamirulloh <amirullohw@gmail.com>
2021-10-27 03:07:14 +0700

    checkpoint

---
+++ frontend/vue-gudangintory/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <video></video>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { QrScanner } from 'qr-scanner';
+
+export default {
+  name: 'Home',
+  components: {
+    QrScanner
+  },
+  data(){
+    
+  },
+  methods: {
+  }
+}
+</script>
